95415ef9ef7d71653bd4880a31311d24ef8bc83e
Manuel Andrés Carrera Galafate <manuelcarrera@acidtango.com>
2024-09-20 16:22:10 +0100

    refactor: deletes unused variables

--- src/CoffeeOrder.ts
+++ src/CoffeeOrder.ts
@@ -4,16 +4,10 @@ import { Cream } from "./complements/Cream.js"
 import { Milk } from "./complements/Milk.js"
 
 export class CoffeeOrder {
-  private hasMilk: boolean = false
-  private hasCream: boolean = false
-
   private complements: Complement[] = []
 
   price() {
     const coffee = new Coffee()
-
-    const milk = this.hasMilk ? new Milk() : undefined
-    const cream = this.hasCream ? new Cream() : undefined
 
     const complementsPrices = this.complements.reduce(
       (complementsSum, nextComplement) => complementsSum + nextComplement.price(),
@@ -25,13 +19,10 @@ export class CoffeeOrder {
 
   withMilk() {
     this.complements.push(new Milk())
-    this.hasMilk = true
   }
 
   withMilkAndCream() {
     this.complements.push(new Milk())
     this.complements.push(new Cream())
-    this.hasMilk = true
-    this.hasCream = true
   }
 }
