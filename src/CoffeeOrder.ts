93bd3db28203030380a032cbef80feca25678dd1
Manuel Andrés Carrera Galafate <manuelcarrera@acidtango.com>
2024-09-20 16:21:46 +0100

    refactor: changes to array of complements

--- src/CoffeeOrder.ts
+++ src/CoffeeOrder.ts
@@ -1,4 +1,5 @@
 import { Coffee } from "./beverages/Coffee.js"
+import { Complement } from "./complements/Complement.js"
 import { Cream } from "./complements/Cream.js"
 import { Milk } from "./complements/Milk.js"
 
@@ -6,20 +7,30 @@ export class CoffeeOrder {
   private hasMilk: boolean = false
   private hasCream: boolean = false
 
+  private complements: Complement[] = []
+
   price() {
     const coffee = new Coffee()
 
     const milk = this.hasMilk ? new Milk() : undefined
     const cream = this.hasCream ? new Cream() : undefined
 
-    return coffee.price() + (milk ? milk.price() : 0) + (cream ? cream.price() : 0)
+    const complementsPrices = this.complements.reduce(
+      (complementsSum, nextComplement) => complementsSum + nextComplement.price(),
+      0,
+    )
+
+    return coffee.price() + complementsPrices
   }
 
   withMilk() {
+    this.complements.push(new Milk())
     this.hasMilk = true
   }
 
   withMilkAndCream() {
+    this.complements.push(new Milk())
+    this.complements.push(new Cream())
     this.hasMilk = true
     this.hasCream = true
   }
