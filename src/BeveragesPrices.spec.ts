5dae4ee0e209b3c9855f7fa0b1d47fc5d607386e
Daniel Montesino <danielmontesino@acidtango.com>
2024-09-20 15:48:57 +0100

    refactor: group describe tests

--- src/BeveragesPrices.spec.ts
+++ src/BeveragesPrices.spec.ts
@@ -8,38 +8,44 @@ import { Tea } from "./Tea.js"
 import { TeaWithMilk } from "./TeaWithMilk.js"
 
 describe("Beverages Prices", () => {
-  it("computes coffee price", () => {
-    const coffee = new Coffee()
-    expect(coffee.price()).toBeCloseTo(1.2)
+  describe("base beverage cases", () => {
+    it("computes coffee price", () => {
+      const coffee = new Coffee()
+      expect(coffee.price()).toBeCloseTo(1.2)
+    })
+
+    it("computes tea price", () => {
+      const tea = new Tea()
+      expect(tea.price()).toBeCloseTo(1.5)
+    })
+
+    it("computes hot chocolate price", () => {
+      const hotChocolate = new HotChocolate()
+      expect(hotChocolate.price()).toBeCloseTo(1.45)
+    })
   })
 
-  it("computes tea price", () => {
-    const tea = new Tea()
-    expect(tea.price()).toBeCloseTo(1.5)
+  describe("beverages with milk cases (+10 cents)", () => {
+    it("computes tea with milk price", () => {
+      const teaWithMilk = new TeaWithMilk()
+      expect(teaWithMilk.price()).toBeCloseTo(1.6)
+    })
+
+    it("computes coffee with milk price", () => {
+      const coffeeWithMilk = new CoffeeWithMilk()
+      expect(coffeeWithMilk.price()).toBeCloseTo(1.3)
+    })
   })
 
-  it("computes hot chocolate price", () => {
-    const hotChocolate = new HotChocolate()
-    expect(hotChocolate.price()).toBeCloseTo(1.45)
-  })
+  describe("beverages with cream cases (+10 cents)", () => {
+    it("computes coffee with milk and cream price", () => {
+      const coffeeWithMilkAndCream = new CoffeeWithMilkAndCream()
+      expect(coffeeWithMilkAndCream.price()).toBeCloseTo(1.45)
+    })
 
-  it("computes tea with milk price", () => {
-    const teaWithMilk = new TeaWithMilk()
-    expect(teaWithMilk.price()).toBeCloseTo(1.6)
-  })
-
-  it("computes coffee with milk price", () => {
-    const coffeeWithMilk = new CoffeeWithMilk()
-    expect(coffeeWithMilk.price()).toBeCloseTo(1.3)
-  })
-
-  it("computes coffee with milk and cream price", () => {
-    const coffeeWithMilkAndCream = new CoffeeWithMilkAndCream()
-    expect(coffeeWithMilkAndCream.price()).toBeCloseTo(1.45)
-  })
-
-  it("computes hot chocolate with cream price", () => {
-    const hotChocolateWithCream = new HotChocolateWithCream()
-    expect(hotChocolateWithCream.price()).toBeCloseTo(1.6)
+    it("computes hot chocolate with cream price", () => {
+      const hotChocolateWithCream = new HotChocolateWithCream()
+      expect(hotChocolateWithCream.price()).toBeCloseTo(1.6)
+    })
   })
 })
